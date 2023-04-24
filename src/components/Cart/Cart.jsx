35622ddcd76218b85871313af661e89c01e8fd4e
nusratBD <nusrat.hurain15@gmail.com>
2023-04-24 11:57:23 +0600

    if-else conditional rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,28 @@
-const Cart = ({carts}) => {
+import './Cart.css';
+const Cart = ({ carts }) => {
+//Conditional Rendering:
+    //1. if-else
+    let message;
+    if (carts.length === 0) {
+        message = <p className="alert-to-buy">Please Buy Some Product</p>
+    }
+    else if(carts.length<=3){
+        message = <p className="alert-to-buy-more">Buy More</p>
+    }
+    else{
+        message = <p className="thanks-alert">Thanks For Shopping!!</p>
+    }
+
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summary</h1>
+            {message}
             <ol>
-            {carts.map(cart=>
-                <li>{cart.name}</li>
-            )}
+                {carts.map(cart =>
+                    <li>{cart.name}</li>
+                )}
             </ol>
-       </div>
+        </div>
     );
 };
 
